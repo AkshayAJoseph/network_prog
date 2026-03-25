f99a68896cc3ed3cd0b58cbd99398a8ff8dbc287
Akshay Joseph <149903511+AkshayAJoseph@users.noreply.github.com>
2026-03-25 08:40:41 +0530

    Implement TCP client to connect and communicate
    This program creates a TCP client that connects to a server at localhost on port 8080, sends a message, and prints the server's response.

---
+++ tcpc.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <string.h>
+
+int main() {
+    int sock = 0;
+    struct sockaddr_in serv_addr;
+    char buffer[1024] = {0};
+
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(8080);
+    
+    inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr);
+
+    connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
+    printf("Connected to server.\n");
+
+    char *msg = "Hello from Client!";
+    send(sock, msg, strlen(msg), 0);
+    
+    read(sock, buffer, 1024);
+    printf("Server replied: %s\n", buffer);
+
+    close(sock);
+    return 0;
+}
