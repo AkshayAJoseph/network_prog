c36fad54c11abe91910be6d4111ba8f0f80e3e6c
Akshay Joseph <149903511+AkshayAJoseph@users.noreply.github.com>
2026-03-25 08:39:47 +0530

    Refactor TCP server implementation in tcps.c

--- tcps.c
+++ tcps.c
@@ -1,50 +1,42 @@
-#include <netinet/in.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include <sys/socket.h>
+#include <netinet/in.h>
 #include <unistd.h>
-#define PORT 8080
-int main(int argc, char const* argv[])
-{
-int server_fd,new_socket,valread;
-struct sockaddr_in address; int opt=1;
-int addrlen=sizeof(address);
-char buffer[1024]={0};
-char* hello="HELLO FROM SERVER";
-//creating socket file descriptor
-if((server_fd=socket(AF_INET,SOCK_STREAM,0))==0)
-{ perror("socket failed");
-exit(EXIT_FAILURE);
-}
-//forcefully attaching socket to te port 8080
-if(setsockopt(server_fd,SOL_SOCKET,SO_REUSEADDR |
-SO_REUSEPORT,&opt,sizeof(opt)))
-{
-perror("setsockopt");
-exit(EXIT_FAILURE);
-}
-address.sin_family=AF_INET;
-address.sin_addr.s_addr=INADDR_ANY;
-address.sin_port=htons(PORT);
-//forcefully attaching socket to the port 8080
-if(bind(server_fd,(struct sockaddr*)&address,sizeof(address))<0)
-{
-perror("bind filed");
-exit(EXIT_FAILURE);
-}
-if(listen(server_fd,4)<0)
-{ perror("listen");
-exit(EXIT_FAILURE);
-}
-if((new_socket=accept(server_fd,(struct sockaddr*)&address,(socklen_t*)&addrlen))<0)
-{
-perror("accept");
-exit(EXIT_FAILURE);
-}
-valread=read(new_socket,buffer,1024);
-printf("%s\n",buffer);
-send(new_socket,hello,strlen(hello),0);
-printf("Hello message sent\n");
-return 0;
+#include <string.h>
+
+int main() {
+    int server_fd, new_socket;
+    struct sockaddr_in address;
+    int addrlen = sizeof(address);
+    char buffer[1024] = {0};
+
+    // 1. Create the socket (The Phone)
+    server_fd = socket(AF_INET, SOCK_STREAM, 0);
+
+    // 2. Define the address and port (The Phone Number)
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY; // Listen on any available network interface
+    address.sin_port = htons(8080);
+
+    // 3. Bind the socket to the port
+    bind(server_fd, (struct sockaddr *)&address, sizeof(address));
+
+    // 4. Start listening for incoming calls (Queue size of 3)
+    listen(server_fd, 3);
+    printf("Server is waiting for a connection...\n");
+
+    // 5. Accept the call (This "blocks" until a client connects)
+    new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen);
+    printf("Connection established!\n");
+
+    // 6. Communication
+    read(new_socket, buffer, 1024);
+    printf("Client says: %s\n", buffer);
+    
+    char *reply = "Hello from Server!";
+    send(new_socket, reply, strlen(reply), 0);
+
+    close(new_socket);
+    close(server_fd);
+    return 0;
 }
